fa758de082b9ffe7ce1e96304f65711b2ab0fa32
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-08-19 13:38:05 +0200

    Add passwords validation based on a pattern and if they match or not

--- src/formValidationHandler.js
+++ src/formValidationHandler.js
@@ -1,3 +1,27 @@
+function areValueMissing(inputsContainer) {
+  let valuesMissing = false;
+  inputsContainer.forEach((input) => {
+    if (input.value.length <= 0) {
+      input.setCustomValidity('This field is required!');
+      valuesMissing = true;
+    }
+  });
+  return valuesMissing;
+}
+
+function checkValueMissingOnInput(inputsContainer) {
+  inputsContainer.forEach((input) => {
+    input.addEventListener('input', () => {
+      console.log('inputting');
+      areValueMissing(inputsContainer);
+    });
+  });
+}
+
+function checkValueMissingOnSubmit(inputsContainer) {
+  areValueMissing(inputsContainer);
+}
+
 function checkValidEmailInput(emailInput) {
   emailInput.addEventListener('input', () => {
     let isValid = false;
@@ -45,29 +69,62 @@ function checkValidZIPCode(ZIPInput) {
   });
 }
 
-function checkCorrectPwdFormat(pwdInput) {
+function isValidPwdFormat(pwdInput) {
   const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   return pattern.test(pwdInput.value);
 }
 
-// function checkValidPwd(pwdInput) {
-// }
+function checkValidPwd(pwdInput) {
+  pwdInput.addEventListener('input', () => {
+    let isValid = false;
+
+    if (!isValidPwdFormat(pwdInput)) {
+      pwdInput.setCustomValidity(
+        'Invalid password format! Make sure your password is atleast 8 characters long, has atleast one uppercase letter, one lowercase letter and one number (eg: h!!oWr2983?@$).',
+      );
+    } else {
+      pwdInput.setCustomValidity('');
+      isValid = true;
+    }
+
+    return isValid;
+  });
+}
+
+function checkPwdsMatch(pwdInput, confirmPwdInput) {
+  confirmPwdInput.addEventListener('input', () => {
+    let areMatching = false;
+
+    if (pwdInput.value !== confirmPwdInput.value) {
+      confirmPwdInput.setCustomValidity('Passwords don\'t match!');
+    } else {
+      confirmPwdInput.setCustomValidity('');
+      areMatching = true;
+    }
+
+    return areMatching;
+  });
+}
 
 function handleForm() {
   const formContainer = document.querySelector('.form-container');
+  const inputsContainer = formContainer.querySelectorAll('.input-container > input');
   const emailInput = formContainer.querySelector('#mail');
   const countryInput = formContainer.querySelector('#country');
   const ZIPInput = formContainer.querySelector('#ZIP');
   const pwdInput = formContainer.querySelector('#pwd');
   const confirmPwdInput = formContainer.querySelector('#confirm-pwd');
+  const submitBtn = formContainer.querySelector('button[type="submit"]');
 
   checkValidEmailInput(emailInput);
   checkValidCountryInput(countryInput);
   checkValidZIPCode(ZIPInput);
-
-  pwdInput.addEventListener('input', () => {
-    console.log(checkCorrectPwdFormat(pwdInput));
+  checkValidPwd(pwdInput);
+  checkPwdsMatch(pwdInput, confirmPwdInput);
+  submitBtn.addEventListener('click', () => {
+    checkValueMissingOnSubmit(inputsContainer);
   });
+  checkValueMissingOnInput(inputsContainer);
 }
 
 export default handleForm;
